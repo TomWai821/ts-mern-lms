4e48a275885924ffbf2c71a60b04dc75fc69c66c
TomWai821 <waichueng821@gmail.com>
2026-03-08 13:28:41 +0800

    CI: rewrite the eslint.config.js for avoid lint error

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,28 +1,24 @@
-
 const eslintPluginTs = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
 
 module.exports = [
-    {
-        files: ['**/*.ts', '**/*.tsx'],
-        languageOptions: 
-        {
-          parser: tseslint.parser,
-          globals: 
-          {
-            require: "readonly",
-            module: "readonly",
-            __dirname: "readonly",
-          },
-        },
-        plugins: 
-        {
-          '@typescript-eslint': eslintPluginTs,
-        },
-        rules: 
-        {
-          "no-unused-vars": "warn",
-          "no-console": "off",
-          "@typescript-eslint/no-require-imports": "off",
-        },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      globals: {
+        require: "readonly",
+        module: "readonly",
+        __dirname: "readonly",
+      },
     },
+    plugins: {
+      '@typescript-eslint': eslintPluginTs,
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  },
 ];
