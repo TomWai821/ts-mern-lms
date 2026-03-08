9b78aad4785b1e1f08e6401e0e26e2acbc0279ca
TomWai821 <waichueng821@gmail.com>
2026-03-08 13:24:04 +0800

    CI: remove ts import syntax in eslint.config.js

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,37 +1,28 @@
 
 const eslintPluginTs = require('@typescript-eslint/eslint-plugin');
-const eslintParserTs = require('@typescript-eslint/parser');
 
 module.exports = [
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    languageOptions: {
-      parser: tseslint.parser,
-      globals: {
-        require: "readonly",
-        module: "readonly",
-        __dirname: "readonly",
-      },
+    {
+        files: ['**/*.ts', '**/*.tsx'],
+        languageOptions: 
+        {
+          parser: tseslint.parser,
+          globals: 
+          {
+            require: "readonly",
+            module: "readonly",
+            __dirname: "readonly",
+          },
+        },
+        plugins: 
+        {
+          '@typescript-eslint': eslintPluginTs,
+        },
+        rules: 
+        {
+          "no-unused-vars": "warn",
+          "no-console": "off",
+          "@typescript-eslint/no-require-imports": "off",
+        },
     },
-    plugins: {
-      '@typescript-eslint': eslintPluginTs,
-    },
-    rules: {
-      "no-unused-vars": "warn",
-      "no-console": "off",
-      "@typescript-eslint/no-require-imports": "off",
-    },
-  },
-];
-
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-
-export default [
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-    
-  },
 ];
