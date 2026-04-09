b3bb8c38fd4c66899bba90a00912bfe063818dd3
TomWai821 <waichueng821@gmail.com>
2026-04-09 15:02:46 +0800

    backend:add request logging and verify path mapping

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,10 +2,18 @@ import express from 'express';
 import cors from 'cors';
 import { routerHandler } from './routes';
 
+
 const ORIGIN_URI = process.env.ORIGIN_URI || "";
 const avaliable_ORIGIN_URI = ORIGIN_URI ? ORIGIN_URI.split(",").map(url => url.trim()) : [];
 
 const app = express();
+
+app.use((req, res, next) => 
+{
+    console.log(`Incoming Request: ${req.method} ${req.url}`);
+    console.log(`Headers: ${JSON.stringify(req.headers)}`);
+    next();
+});
 
 app.use(cors(
     {
